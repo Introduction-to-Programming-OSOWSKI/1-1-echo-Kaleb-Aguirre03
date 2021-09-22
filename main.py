8cc3984bcc87f109bbd0ea27cd0aba6582859e27
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-22 09:36:42 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+#Define the funtion echo
+def echo (a, b):
+
+    #return variable a b times
+    return a * b
+
+#print the funtion in order to obtain yo 2 times
+print (echo ("yo", 2))
+
+#print the funtion in order to obtain echo 5 times
+print (echo ("echo", 5))
